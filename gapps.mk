0221c929ffd48d58bbf15ef92c5238734c8326c5
bieltv3 <bieltv.3@gmail.com>
2012-12-14 15:06:34 +0100

    add correct directory for GApps

--- gapps.mk
+++ gapps.mk
@@ -1,17 +1,17 @@
 # Google Apps
 PRODUCT_COPY_FILES += \
-	device/samsung/cooper/prebuilt/system/app/Gmail.apk:system/app/Gmail.apk \
-	device/samsung/cooper/prebuilt/system/app/GoogleBackupTransport.apk:system/app/GoogleBackupTransport.apk \
-	device/samsung/cooper/prebuilt/system/app/GoogleCalendarSyncAdapter.apk:system/app/GoogleCalendarSyncAdapter.apk \
-	device/samsung/cooper/prebuilt/system/app/GoogleContactsSyncAdapter.apk:system/app/GoogleContactsSyncAdapter.apk \
-	device/samsung/cooper/prebuilt/system/app/GoogleLoginService.apk:system/app/GoogleLoginService.apk \
-	device/samsung/cooper/prebuilt/system/app/GoogleServicesFramework.apk:system/app/GoogleServicesFramework.apk \
-	device/samsung/cooper/prebuilt/system/app/PlayStore.apk:system/app/PlayStore.apk \
-	device/samsung/cooper/prebuilt/system/app/NetworkLocation.apk:system/app/NetworkLocation.apk
+	device/samsung/cooper/prebuilt/app/Gmail.apk:system/app/Gmail.apk \
+	device/samsung/cooper/prebuilt/app/GoogleBackupTransport.apk:system/app/GoogleBackupTransport.apk \
+	device/samsung/cooper/prebuilt/app/GoogleCalendarSyncAdapter.apk:system/app/GoogleCalendarSyncAdapter.apk \
+	device/samsung/cooper/prebuilt/app/GoogleContactsSyncAdapter.apk:system/app/GoogleContactsSyncAdapter.apk \
+	device/samsung/cooper/prebuilt/app/GoogleLoginService.apk:system/app/GoogleLoginService.apk \
+	device/samsung/cooper/prebuilt/app/GoogleServicesFramework.apk:system/app/GoogleServicesFramework.apk \
+	device/samsung/cooper/prebuilt/app/PlayStore.apk:system/app/PlayStore.apk \
+	device/samsung/cooper/prebuilt/app/NetworkLocation.apk:system/app/NetworkLocation.apk
 
 PRODUCT_COPY_FILES += \
-        device/samsung/cooper/prebuilt/system/etc/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
-        device/samsung/cooper/prebuilt/system/etc/permissions/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml \
-        device/samsung/cooper/prebuilt/system/etc/permissions/com.google.widevine.software.drm.xml:system/etc/permissions/com.google.widevine.software.drm.xml \
-        device/samsung/cooper/prebuilt/system/etc/g.prop:system/etc/g.prop
+        device/samsung/cooper/prebuilt/etc/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
+        device/samsung/cooper/prebuilt/etc/permissions/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml \
+        device/samsung/cooper/prebuilt/etc/permissions/com.google.widevine.software.drm.xml:system/etc/permissions/com.google.widevine.software.drm.xml \
+        device/samsung/cooper/prebuilt/etc/g.prop:system/etc/g.prop
 
